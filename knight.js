c4ffc7a91bebbf88d2d5f3010bfc73ea9334af59
Sean <seanpoulter@hotmail.com>
2022-11-11 01:00:39 -0500

    Add notes

--- knight.js
+++ knight.js
@@ -1,14 +1,22 @@
 /* 
-
-
+loop from 0 to < 8 for boundary
+use knightOption[0] at first until unable to hit end square and then backtrack to prev location and use next knightOption
 
 */
 
+function moves() {
+    const knightOptions = [
+        [1, 2], [2, 1],
+        [-1, 2], [2, -1],
+        [1, -2], [-2, 1],
+        [-1, -2], [-2, -1]
+    ]
 
-function createGameBoard() {
 
 }
 
 function knightTravails() {
 
 }
+
+module.exports = knightTravails
